7cc3c86d21bafae39fc4d65b07767d4f8813b1c9
dcher <cernookijdima7@gmail.com>
2023-10-15 17:25:06 +0300

    1

--- src/main/java/edu/hw1/Main.java
+++ src/main/java/edu/hw1/Main.java
@@ -18,9 +18,6 @@ public final class Main {
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 0; i <= 2; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
             LOGGER.info("i = {}", i);
         }
     }
